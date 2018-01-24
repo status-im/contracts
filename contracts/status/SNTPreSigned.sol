d12683b9b4b5226220c0527d7008c332cf19b6b3
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2018-01-24 02:51:49 +0000

    presigned token

---
+++ contracts/status/SNTPreSigned.sol
@@ -0,0 +1,26 @@
+pragma solidity ^0.4.11;
+
+import "../token/MiniMeTokenPreSigned.sol";
+import "../token/MiniMeTokenPreSignedFactory.sol";
+
+/*
+    Copyright 2017, Jarrad Hope (Status Research & Development GmbH)
+*/
+
+contract SNTPreSigned is MiniMeTokenPreSigned {
+    // @dev SNT constructor just parametrizes the MiniMeIrrevocableVestedToken constructor
+    function SNTPreSigned(address _oldToken)
+            MiniMeToken(
+                new MiniMeTokenPreSignedFactory(),
+                _oldToken,                     // parent token
+                block.number,                       // snapshot block
+                "Status Network Token",  // Token name
+                18,                      // Decimals
+                "SNT",                   // Symbol
+                true                     // Enable transfers
+            ) 
+                public 
+            {
+
+            }
+}
