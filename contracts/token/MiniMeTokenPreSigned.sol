00627c82589b9da583ebf5a0d27ba7a2fddd8e68
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2018-02-02 17:27:58 -0200

    back to ordered nonces, vars name normalization, support of full signatures, approvaandcallfallback warning fix

--- contracts/token/MiniMeTokenPreSigned.sol
+++ contracts/token/MiniMeTokenPreSigned.sol
@@ -31,7 +31,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         
     }
     
-    mapping (address => mapping(uint256 => bool)) public nonce;
+    mapping (address => uint256) public nonce;
 
     /**
      * @notice Include a presigned `transfer(address,uint256)`
@@ -39,7 +39,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
      * @param _sigR Signature R
      * @param _sigS Signature S
      * @param _to The address of the recipient
-     * @param _value The amount of tokens to be transferred
+     * @param _value The value of tokens to be transferred
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
      */
@@ -57,10 +57,10 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         uint256 _gas = msg.gas;
         address recovered = recoverTransferPreSigned(_sigV, _sigR, _sigS, _to, _value, _gasPrice, _nonce);
         require(recovered > 0x0);
-        if (nonce[recovered][_nonce]) {
+        if (nonce[recovered] != _nonce) {
             return;
         }
-        nonce[recovered][_nonce] = true;
+        nonce[recovered]++;
         require(doTransfer(recovered, _to, _value));
         _gas = 21000 + (_gas - msg.gas);
         if (_gasPrice > 0) {
@@ -73,8 +73,8 @@ contract MiniMeTokenPreSigned is MiniMeToken {
      * @param _sigV Signature V 
      * @param _sigR Signature R
      * @param _sigS Signature S
-     * @param _spender The address of the recipient
-     * @param _amount The amount of tokens to be transferred
+     * @param _to The address of the recipient
+     * @param _value The value of tokens to be transferred
      * @param _extraData option data to send to contract
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
@@ -83,8 +83,8 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         uint8 _sigV,
         bytes32 _sigR,
         bytes32 _sigS,
-        address _spender,
-        uint256 _amount,
+        address _to,
+        uint256 _value,
         bytes _extraData,
         uint256 _gasPrice,
         uint256 _nonce
@@ -93,23 +93,23 @@ contract MiniMeTokenPreSigned is MiniMeToken {
     {
         uint256 _gas = msg.gas;
         require(transfersEnabled);
-        address recovered = recoverApproveAndCallPreSigned(_sigV, _sigR, _sigS, _spender, _amount, _extraData, _gasPrice, _nonce);
+        address recovered = recoverApproveAndCallPreSigned(_sigV, _sigR, _sigS, _to, _value, _extraData, _gasPrice, _nonce);
         require(recovered > 0x0);
 
-        if (nonce[recovered][_nonce]) {
-            return;
+        if (nonce[recovered] != _nonce) {
+            return; //ignore if incorrect nonce (avoid reverting at common case of tx already included)
         }
-        nonce[recovered][_nonce] = true;
+        nonce[recovered]++;
 
-        require((_amount == 0) || (allowed[recovered][_spender] == 0));
+        require((_value == 0) || (allowed[recovered][_to] == 0));
         if (isContract(controller)) {
-            require(TokenController(controller).onApprove(recovered, _spender, _amount));
+            require(TokenController(controller).onApprove(recovered, _to, _value));
         }
-        allowed[recovered][_spender] = _amount;
-        Approval(recovered, _spender, _amount);
-        ApproveAndCallFallBack(_spender).receiveApproval(
+        allowed[recovered][_to] = _value;
+        Approval(recovered, _to, _value);
+        ApproveAndCallFallBack(_to).receiveApproval(
             recovered,
-            _amount,
+            _value,
             this,
             _extraData
         );
@@ -126,7 +126,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
      * @param _sigR Signature R
      * @param _sigS Signature S
      * @param _to The address of the recipient
-     * @param _value The amount of tokens to be transferred
+     * @param _value The value of tokens to be transferred
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
      */
@@ -147,12 +147,83 @@ contract MiniMeTokenPreSigned is MiniMeToken {
             transferPreSigned(_sigV[i], _sigR[i], _sigS[i], _to[i], _value[i], _gasPrice[i], _nonce[i]);
         }
     }
+
+    /**
+     * @notice Include a presigned `transfer(address,uint256)`
+     * @param _signature Signed transfer 
+     * @param _to The address of the recipient
+     * @param _value The value of tokens to be transferred
+     * @param _gasPrice How much tokens willing to pay per gas
+     * @param _nonce Presigned transaction number.
+     */
+    function transferPreSigned(
+        bytes _signature,
+        address _to,
+        uint256 _value,
+        uint256 _gasPrice,
+        uint256 _nonce
+    )
+        public
+    {
+        var (sigV, sigR, sigS) = signatureSplit(_signature);
+        transferPreSigned(sigV, sigR, sigS, _to, _value, _gasPrice, _nonce);
+    }
     
+    /**
+     * @notice Include a presigned `approveAndCall(address,uint256,bytes)`
+     * @param _sigV Signature V 
+     * @param _sigR Signature R
+     * @param _sigS Signature S
+     * @param _to The address of the recipient
+     * @param _value The value of tokens to be transferred
+     * @param _extraData option data to send to contract
+     * @param _gasPrice How much tokens willing to pay per gas
+     * @param _nonce Presigned transaction number.
+     */
+    function approveAndCallPreSigned(
+        bytes _signature,
+        address _to,
+        uint256 _value,
+        bytes _extraData,
+        uint256 _gasPrice,
+        uint256 _nonce
+    )
+        public
+    {
+        var (sigV, sigR, sigS) = signatureSplit(_signature);
+        approveAndCallPreSigned(sigV, sigR, sigS, _to, _value, _extraData, _gasPrice, _nonce);
+    }    
+    
+    
+    /**
+     * @dev divides bytes signature into `uint8 v, bytes32 r, bytes32 s` 
+     */
+    function signatureSplit(bytes signature)
+        pure
+        public
+        returns (uint8 v, bytes32 r, bytes32 s)
+    {
+        // The signature format is a compact form of:
+        //   {bytes32 r}{bytes32 s}{uint8 v}
+        // Compact means, uint8 is not padded to 32 bytes.
+        assembly {
+            r := mload(add(signature, 32))
+            s := mload(add(signature, 64))
+            // Here we are loading the last 32 bytes, including 31 bytes
+            // of 's'. There is no 'mload8' to do this.
+            //
+            // 'byte' is not working due to the Solidity parser, so lets
+            // use the second best option, 'and'
+            v := and(mload(add(signature, 65)), 0xff)
+        }
+
+        require(v == 27 || v == 28);
+    }
     
     /**
      * @notice Gets txHash for using as Presigned Transaction
      * @param _to The address of the recipient
-     * @param _value The amount of tokens to be transferred
+     * @param _value The value of tokens to be transferred
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
      * @return txHash 'transferPreSigned(uint8,bytes32,bytes32,address,uint256,uint256,uint256)' hash
@@ -174,16 +245,16 @@ contract MiniMeTokenPreSigned is MiniMeToken {
    
     /**
      * @notice get txHash of presigned `approveAndCallPreSigned(uint8,bytes32,bytes32,address,uint256,bytes,uint256,uint256)`
-     * @param _spender The address of the recipient
-     * @param _amount The amount of tokens to be transferred
+     * @param _to The address of the recipient
+     * @param _value The value of tokens to be transferred
      * @param _extraData option data to send to contract
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
      * @return txHash approveAndCallPreSigned(uint8,bytes32,bytes32,address,uint256,bytes,uint256,uint256) hash 
      */
     function getApproveAndCallHash(
-        address _spender,
-        uint256 _amount,
+        address _to,
+        uint256 _value,
         bytes _extraData,
         uint256 _gasPrice,
         uint256 _nonce
@@ -193,7 +264,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         returns(bytes32 txHash)
     {
         //"c6a08009": "approveAndCallPreSigned(uint8,bytes32,bytes32,address,uint256,bytes,uint256,uint256)"
-        txHash = keccak256(address(this), bytes4(0xc6a08009), _spender, _amount, _extraData, _gasPrice, _nonce);
+        txHash = keccak256(address(this), bytes4(0xc6a08009), _to, _value, _extraData, _gasPrice, _nonce);
     }
 
     /**
@@ -218,7 +289,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
      * @param _sigR Signature R
      * @param _sigS Signature S
      * @param _to The address of the recipient
-     * @param _value The amount of tokens to be transferred
+     * @param _value The value of tokens to be transferred
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
      */
@@ -243,8 +314,8 @@ contract MiniMeTokenPreSigned is MiniMeToken {
      * @param _sigV Signature V 
      * @param _sigR Signature R
      * @param _sigS Signature S
-     * @param _spender The address of the recipient
-     * @param _amount The amount of tokens to be transferred
+     * @param _to The address of the recipient
+     * @param _value The value of tokens to be transferred
      * @param _extraData option data to send to contract
      * @param _gasPrice How much tokens willing to pay per gas
      * @param _nonce Presigned transaction number.
@@ -253,8 +324,8 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         uint8 _sigV,
         bytes32 _sigR,
         bytes32 _sigS,
-        address _spender,
-        uint256 _amount,
+        address _to,
+        uint256 _value,
         bytes _extraData,
         uint256 _gasPrice,
         uint256 _nonce
@@ -263,6 +334,7 @@ contract MiniMeTokenPreSigned is MiniMeToken {
         public
         returns (address recovered)
     {
-        recovered = ecrecover(getSignHash(getApproveAndCallHash(_spender, _amount, _extraData, _gasPrice, _nonce)), _sigV, _sigR, _sigS);
+        recovered = ecrecover(getSignHash(getApproveAndCallHash(_to, _value, _extraData, _gasPrice, _nonce)), _sigV, _sigR, _sigS);
     }
+
 }
