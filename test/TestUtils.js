06628093bcfe90420c6e1b01542bb38bdac406ce
Richard Ramos <info@richardramos.me>
2018-02-21 17:17:38 -0400

    - Updated logic on Identity.sol to reflect changes on EIPs - Added migration to deploy Identity contract - Updated existing test cases to reflect changes on contract interface

--- test/TestUtils.js
+++ test/TestUtils.js
@@ -32,3 +32,34 @@ exports.listenForEvent = event => new Promise((resolve, reject) => {
         event.stopWatching()
     })
 })
+
+
+exports.addressToBytes32 = (address) => {
+    const stringed = "0000000000000000000000000000000000000000000000000000000000000000" + address.slice(2);
+    return "0x" + stringed.substring(stringed.length - 64, stringed.length); 
+}
+
+
+// OpenZeppelin's expectThrow helper -
+// Source: https://github.com/OpenZeppelin/zeppelin-solidity/blob/master/test/helpers/expectThrow.js
+exports.expectThrow = async promise => {
+    try {
+      await promise;
+    } catch (error) {
+      // TODO: Check jump destination to destinguish between a throw
+      //       and an actual invalid jump.
+      const invalidOpcode = error.message.search('invalid opcode') >= 0;
+      // TODO: When we contract A calls contract B, and B throws, instead
+      //       of an 'invalid jump', we get an 'out of gas' error. How do
+      //       we distinguish this from an actual out of gas event? (The
+      //       testrpc log actually show an 'invalid jump' event.)
+      const outOfGas = error.message.search('out of gas') >= 0;
+      const revert = error.message.search('revert') >= 0;
+      assert(
+        invalidOpcode || outOfGas || revert,
+        'Expected throw, got \'' + error + '\' instead',
+      );
+      return;
+    }
+    assert.fail('Expected throw not received');
+  };
--- test/identity.js
+++ test/identity.js
@@ -4,18 +4,18 @@ const Identity = artifacts.require("./identity/Identity.sol");
 contract('Identity', function(accounts) {
 
     let identity;
+
     beforeEach(async () => {
         identity = await Identity.new({from: accounts[0]})
     })
 
 
     describe("Identity()", () => {
-
         it("initialize with msg.sender as management key", async () => {
             assert.equal(
-                await identity.getKeyType(accounts[0]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[0])),
                 1,
-                identity.address+".getKeyType("+accounts[0]+") is not MANAGEMENT_KEY")
+                identity.address + ".getKeyPurpose("+accounts[0]+") is not MANAGEMENT_KEY")
         });
         
     });
@@ -24,33 +24,33 @@ contract('Identity', function(accounts) {
     describe("addKey(address _key, uint256 _type)", () => {
 
         it("MANAGEMENT_KEY add a new address as ACTION_KEY", async () => {
-            await identity.addKey(accounts[1], 2, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 2, 1, {from: accounts[0]})
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 2,
-                identity.address+".getKeyType("+accounts[1]+") is not ACTION_KEY")
+                identity.address+".getKeyPurpose("+accounts[1]+") is not ACTION_KEY")
         });
 
 
         it("should not add key by non manager", async () => {            
             try {
-                await identity.addKey(accounts[1], 1, {from: accounts[2]})
+                await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 1, 1, {from: accounts[2]})
             }catch(e){
             }
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 0,
-                identity.address+".getKeyType("+accounts[1]+") is not correct")
+                identity.address+".getKeyPurpose("+accounts[1]+") is not correct")
         });
 
         it("should not add key type 1 by actor", async () => {            
-            await identity.addKey(accounts[2], 2, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[2]), 2, 1, {from: accounts[0]})
             try {
-                await identity.addKey(accounts[1], 1, {from: accounts[2]})
+                await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 1, 1, {from: accounts[2]})
             } catch(e){
             }
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 0,
                 identity.address+".getKeyType("+accounts[1]+") is not correct")
         });
@@ -58,9 +58,9 @@ contract('Identity', function(accounts) {
 
 
         it("fire KeyAdded(address indexed key, uint256 indexed type)", async () => {
-            identity.addKey(accounts[1], 2, {from: accounts[0]})
+            identity.addKey(TestUtils.addressToBytes32(accounts[1]), 2, 1, {from: accounts[0]})
             const keyAdded = await TestUtils.listenForEvent(identity.KeyAdded())
-            assert(keyAdded.key, accounts[1], "Key is not correct")
+            assert(keyAdded.key, TestUtils.addressToBytes32(accounts[1]), "Key is not correct")
             assert(keyAdded.keyType, 2, "Type is not correct")
         });
 
@@ -70,100 +70,101 @@ contract('Identity', function(accounts) {
     describe("removeKey(address _key, uint256 _type)", () => {
 
         it("MANAGEMENT_KEY should removes a key", async () => {
-            await identity.addKey(accounts[1], 1, {from: accounts[0]})
-            await identity.removeKey(accounts[0], {from: accounts[1]})    
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 1, 1, {from: accounts[0]})
+            await identity.removeKey(TestUtils.addressToBytes32(accounts[0]), 1, {from: accounts[1]})    
             assert.equal(
-                await identity.getKeyType(accounts[0]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[0])),
                 0,
-                identity.address+".getKeyType("+accounts[0]+") is not 0")
+                identity.address+".getKeyPurpose("+accounts[0]+") is not 0")
         });
         
 
         it("other key should not removes a key", async () => {
-            await identity.addKey(accounts[1], 1, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 1, 1, {from: accounts[0]})
             try {
-                await identity.removeKey(accounts[1], {from: accounts[2]})    
+                await identity.removeKey(TestUtils.addressToBytes32(accounts[1]), 1, {from: accounts[2]})    
             }catch (e) {
 
             }
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 1,
-                identity.address+".getKeyType("+accounts[1]+") is not 0")
+                identity.address+".getKeyPurpose("+accounts[1]+") is not 0")
         });
 
         it("actor key should not remove key", async () => {
-            await identity.addKey(accounts[1], 2, {from: accounts[0]})
-            await identity.addKey(accounts[2], 2, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 2, 1, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[2]), 2, 1, {from: accounts[0]})
             try {
-                await identity.removeKey(accounts[1], {from: accounts[2]})    
+                await identity.removeKey(TestUtils.addressToBytes32(accounts[1]), 1, {from: accounts[2]})    
             }catch (e) {
 
             }
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 2,
                 identity.address+".getKeyType("+accounts[1]+") is not 0")
         });
         
 
         it("MANAGEMENT_KEY should not remove itself MANAGEMENT_KEY when there is no other MANAGEMENT_KEY", async () => {
-            try {
-                await identity.removeKey(accounts[0], {from: accounts[0]})
-            } catch(e) {
-                //nothing
+            let assertJump = (error) => {
+                assert.isAbove(error.message.search('revert'), -1, 'Revert should happen');
             }
-            assert.equal(
-                await identity.getKeyType(accounts[0]),
-                1,
-                identity.address+".getKeyType("+accounts[0]+") is not MANAGEMENT_KEY")
-          
+            try {
+                await identity.removeKey(TestUtils.addressToBytes32(accounts[0]), 1, {from: accounts[0]});
+                assert.fail('should have reverted before');
+              } catch(error) {
+                  assertJump(error);
+              }
+              
+            
         });
 
         it("fire KeyRemoved(address indexed key, uint256 indexed type)", async () => {
-            await identity.addKey(accounts[1], 2, {from: accounts[0]})
-            identity.removeKey(accounts[1], {from: accounts[0]})  
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 2, 1,{from: accounts[0]})
+            identity.removeKey(TestUtils.addressToBytes32(accounts[1]), 2, {from: accounts[0]})  
             const keyRemoved = await TestUtils.listenForEvent(identity.KeyRemoved())
-            assert(keyRemoved.key, accounts[1], "Key is not correct")
+            assert(keyRemoved.key, TestUtils.addressToBytes32(accounts[1]), "Key is not correct")
             assert(keyRemoved.keyType, 2, "Type is not correct")
         });
 
     });
 
 
-    describe("getKeyType(address _key)", () => {
+    describe("getKeyPurpose(address _key)", () => {
 
         it("should start only with initializer as only key", async () => {
             assert.equal(
-                await identity.getKeyType(accounts[0]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[0])),
                 1,
-                identity.address+".getKeyType("+accounts[0]+") is not correct")
+                identity.address+".getKeyPurpose("+accounts[0]+") is not correct")
 
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 0,
-                identity.address+".getKeyType("+accounts[1]+") is not correct")
+                identity.address+".getKeyPurpose("+accounts[1]+") is not correct")
         });
 
         it("should get type 2 after addKey type 2", async () => {
-            await identity.addKey(accounts[1], 2, {from: accounts[0]})
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 2, 1, {from: accounts[0]})
             assert.equal(
-                await identity.getKeyType(accounts[1]),
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
                 2,
-                identity.address+".getKeyType("+accounts[1]+") is not correct")
+                identity.address+".getKeyPurpose("+accounts[1]+") is not correct")
             });
             
-        it("should get type 999 after addKey type 999", async () => {            
-            await identity.addKey(accounts[1], 999, {from: accounts[0]})
+        it("should get type 3 after addKey type 3", async () => {            
+            await identity.addKey(TestUtils.addressToBytes32(accounts[1]), 3, 1, {from: accounts[0]})
             assert.equal(
-                await identity.getKeyType(accounts[1]),
-                999,
-                identity.address+".getKeyType("+accounts[1]+") is not correct")
+                await identity.getKeyPurpose(TestUtils.addressToBytes32(accounts[1])),
+                3,
+                identity.address+".getKeyPurpose("+accounts[1]+") is not correct")
         });
 
     });
 
-
+   
     describe("getKeysByType(uint256 _type)", () => {
 
         it("at initialization", async () => {
@@ -184,30 +185,6 @@ contract('Identity', function(accounts) {
 
     });
 
-
-    describe("replaceKey(address _oldKey, address _newKey)", () => {
-
-        it("MANAGEMENT_KEY replace itself (alone)", async () => {
-        
-        });
-
-        it("MANAGEMENT_KEY replace a key between others", async () => {
-            
-        });
-
-        it("MANAGEMENT_KEY replace the first key", async () => {
-            
-        });
-
-        it("MANAGEMENT_KEY replace the last key", async () => {
-            
-        });
-
-        it("fire KeyReplaced(address indexed oldKey, address indexed newKey, uint256 indexed type)", async () => {
-
-        });
-
-    });
 
 
     describe("execute(address _to, uint256 _value, bytes _data)", () => {
@@ -282,5 +259,5 @@ contract('Identity', function(accounts) {
             
         });
     });
-                
+             
 });
