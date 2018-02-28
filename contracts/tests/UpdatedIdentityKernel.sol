2824b8039a7368254f3596d38e39b4466ab6e2d4
Richard Ramos <info@richardramos.me>
2018-02-28 11:47:59 -0400

    - Added event to inform of an identity instance upgrade - Added test units for Identity factory and its kernel upgrading

---
+++ contracts/tests/UpdatedIdentityKernel.sol
@@ -0,0 +1,20 @@
+pragma solidity ^0.4.17;
+
+import "../identity/IdentityKernel.sol";
+
+contract UpdatedIdentityKernel is IdentityKernel {
+
+    function initIdentity(address _caller) external {
+        require(minimumApprovalsByKeyType[MANAGEMENT_KEY] == 0);
+        _addKey(bytes32(_caller), MANAGEMENT_KEY, 0);
+        minimumApprovalsByKeyType[MANAGEMENT_KEY] = 1;
+    }
+
+    event TestFunctionExecuted();
+
+    function test() public {
+        TestFunctionExecuted();
+    }
+
+    
+}
