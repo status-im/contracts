3c4f2ab7d1a5219b8f36e7659c6b8b22166132d8
Richard Ramos <info@richardramos.me>
2018-02-28 14:52:32 -0400

    Linting to reduce problems in IDE

--- contracts/tests/UpdatedIdentityKernel.sol
+++ contracts/tests/UpdatedIdentityKernel.sol
@@ -2,13 +2,12 @@ pragma solidity ^0.4.17;
 
 import "../identity/IdentityKernel.sol";
 
+
 contract UpdatedIdentityKernel is IdentityKernel {
 
     event TestFunctionExecuted(uint8 minApprovalsByManagementKeys);
 
     function test() public {
         TestFunctionExecuted(minimumApprovalsByKeyType[MANAGEMENT_KEY]);
-    }
-
-    
+    }   
 }
