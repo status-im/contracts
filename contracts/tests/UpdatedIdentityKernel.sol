7560ac83287bedfe9ed49ef6765e3972dcdf097a
Richard Ramos <info@richardramos.me>
2018-02-28 14:39:22 -0400

    Removing unused function

--- contracts/tests/UpdatedIdentityKernel.sol
+++ contracts/tests/UpdatedIdentityKernel.sol
@@ -4,12 +4,6 @@ import "../identity/IdentityKernel.sol";
 
 contract UpdatedIdentityKernel is IdentityKernel {
 
-    function initIdentity(address _caller) external {
-        require(minimumApprovalsByKeyType[MANAGEMENT_KEY] == 0);
-        _addKey(bytes32(_caller), MANAGEMENT_KEY, 0);
-        minimumApprovalsByKeyType[MANAGEMENT_KEY] = 1;
-    }
-
     event TestFunctionExecuted(uint8 minApprovalsByManagementKeys);
 
     function test() public {
