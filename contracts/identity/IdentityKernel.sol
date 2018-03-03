84bb005997e8a0c116a43ccce9cf3c47e5986d9a
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2018-03-03 17:16:32 -0300

    constructor logic encapsulated to prevent programming errors

--- contracts/identity/IdentityKernel.sol
+++ contracts/identity/IdentityKernel.sol
@@ -6,8 +6,6 @@ import "./Identity.sol";
 contract IdentityKernel is InstanceStorage, Identity {
 
     function initIdentity(address _caller) external {
-        require(minimumApprovalsByKeyPurpose[MANAGEMENT_KEY] == 0);
-        _addKey(bytes32(_caller), MANAGEMENT_KEY, 0);
-        minimumApprovalsByKeyPurpose[MANAGEMENT_KEY] = 1;
+        _constructIdentity(_caller);
     }
 }
