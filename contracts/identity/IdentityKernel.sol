e17be07fea35fffd7e979ffcc27767169f4656c0
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2018-03-21 22:14:09 -0300

    kernel use economic abstracted identity

--- contracts/identity/IdentityKernel.sol
+++ contracts/identity/IdentityKernel.sol
@@ -1,9 +1,9 @@
 pragma solidity ^0.4.17;
 
 import "../deploy/DelayedUpdatableInstanceStorage.sol";
-import "./Identity.sol";
+import "./IdentityGasRelay.sol";
 
-contract IdentityKernel is DelayedUpdatableInstanceStorage, Identity {
+contract IdentityKernel is DelayedUpdatableInstanceStorage, IdentityGasRelay {
 
     function initIdentity(address _caller) external {
         _constructIdentity(_caller);
