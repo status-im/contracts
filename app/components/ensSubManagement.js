8fc76d23c3a4407bde914df039373f1fa86ddc62
Barry Gitarts <bgitarts@gmail.com>
2018-05-22 15:44:33 -0400

    add formik with submission handling

--- app/components/ensSubManagement.js
+++ app/components/ensSubManagement.js
@@ -2,6 +2,7 @@ import EmbarkJS from 'Embark/EmbarkJS';
 import ENSSubdomainRegistry from 'Embark/contracts/ENSSubdomainRegistry';
 import React, { Fragment } from 'react';
 import { Form, FormGroup, FormControl, HelpBlock, Button, ControlLabel } from 'react-bootstrap';
+import AddDomain from './ens/addDomain'
 
 const FieldGroup = ({ id, label, help, ...props }) => (
   <FormGroup controlId={id}>
@@ -15,20 +16,7 @@ const ENSSubManagement = (props) => (
   <Fragment>
     <h2>Subdomain management</h2>
     <h3>Add Domain</h3>
-    <form>
-      <FieldGroup
-        id="domainName"
-        type="text"
-        label="Domain Name"
-      />
-      <FieldGroup
-        id="domainPrice"
-        type="number"
-        label="Domain Price"
-        placeholder="(Optional) Domain will be free if left blank"
-      />
-      <Button type="submit">Submit</Button>
-    </form>
+    <AddDomain />
   </Fragment>
 )
 
