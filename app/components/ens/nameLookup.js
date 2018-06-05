b0439c6ce14ff43e631747b0b5b8ac2f78846d5e
Barry Gitarts <bgitarts@gmail.com>
2018-06-05 10:43:05 -0400

    add additional custom ui components + complete name lookup style

--- app/components/ens/nameLookup.js
+++ app/components/ens/nameLookup.js
@@ -1,13 +1,21 @@
 import React, { Fragment } from 'react';
-import Field from '../../ui/components/Field'
-import TextInput from '../../ui/components/TextInput'
+import { Button, Field, TextInput, Card } from '../../ui/components'
+
+const cardStyle = {
+  width: '75%',
+  marginLeft: '15%',
+  padding: '30px'
+}
 
 const NameLookup = () => (
-  <div>
+  <Card style={cardStyle}>
     <Field label="Enter Domain or Status Name" wide>
       <TextInput wide required />
     </Field>
-  </div>
+    <Button mode="strong" type="submit" wide>
+      Get Address
+    </Button>
+  </Card>
 )
 
 export default NameLookup;
--- app/ui/index.js
+++ app/ui/index.js
@@ -1,2 +1,7 @@
-export { default as Card } from './components/Card'
-export { default as Field } from './components/Field.js'
+// @create-index
+
+export { default as components } from './components';
+export { default as providers } from './providers';
+export { default as theme } from './theme';
+export { default as utils } from './utils';
+
