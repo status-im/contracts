cb41a904c5e0fc745052560846c39e0f603eea3a
Barry Gitarts <bgitarts@gmail.com>
2018-06-05 13:19:21 -0400

    add update ERC20 account balance
    add reselect - memoizes from the state tree

--- app/actions/accounts.js
+++ app/actions/accounts.js
@@ -1,3 +1,4 @@
+import ERC20Token from 'Embark/contracts/ERC20Token';
 import { actions as accountActions } from '../reducers/accounts'
 
 const { receiveAccounts } = accountActions
@@ -7,7 +8,8 @@ export const fetchAndDispatchAccountsWithBalances = (web3, dispatch) => {
       const defaultAccount = web3.eth.defaultAccount || addresses[0]
       const accounts = addresses.map(async address => {
         const balance = await web3.eth.getBalance(address, 'latest')
-        return { address, balance }
+        const ERC20TokenBalance = await ERC20Token.methods.balanceOf(address).call()
+        return { address, balance, ERC20TokenBalance }
       })
       Promise.all(accounts).then(accounts => {
         dispatch(receiveAccounts(defaultAccount, accounts))
