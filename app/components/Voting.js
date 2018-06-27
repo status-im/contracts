c775fab955f6545f4526db2231e33a2a4db79457
Barry Gitarts <bgitarts@gmail.com>
2018-06-27 15:08:09 -0400

    add status logo

--- app/components/Voting.js
+++ app/components/Voting.js
@@ -4,11 +4,15 @@ import 'typeface-roboto';
 import AppBar from './standard/AppBar';
 import AddPoll from './simple-voting/AddPoll';
 import PollsList from './simple-voting/PollsList';
+import StatusLogo from '../ui/components/StatusLogo';
 
 export default ({ toggleAdmin, rawPolls }) => (
   <Fragment>
     <CssBaseline />
     <AppBar toggleAdmin={toggleAdmin} />
+    <div style={{ margin: '15px 15px 15px 35%' }}>
+      <StatusLogo />
+    </div>
     <AddPoll />
     {rawPolls && <PollsList rawPolls={rawPolls} />}
   </Fragment>
