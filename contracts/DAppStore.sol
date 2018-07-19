c8994fd7ce4187bc707d6a0da49e94d8718a330f
Andy Tudhope <tuddy0525@gmail.com>
2018-07-19 12:32:07 +0200

    Changes calculations
    This PR changes the way we calculate the cost of minting votes and the number of votes we therefore mint under different conditions.
    
    It is still WIP as figuring out the logic around withdrawals is now quite hard.
    
    Am happy to explain my decisions over a call this week.

--- contracts/DAppStore.sol
+++ contracts/DAppStore.sol
@@ -4,9 +4,8 @@ import './token/MiniMeTokenInterface.sol';
 
 contract DAppStore {
     
-    uint256 constant percentage_num = 1;
-    uint256 constant percentage_den = 100000;
-    // if num = 1, and den = 100000, num/den = 0.000001
+    uint256 constant rate = 1.6;
+    uint256 constant curve = 1 / rate;
 
     MiniMeTokenInterface SNT;
 
@@ -26,6 +25,7 @@ contract DAppStore {
         bytes32 id;
         uint256 SNTBalance;
         uint256 effectiveBalance;
+        uint256 prevMinted;
         Vote[] votes;
     }
 
@@ -52,6 +52,7 @@ contract DAppStore {
         d.name = _name;
         d.id = _id;
         d.SNTBalance = _amountToStake;
+        d.prevMinted = _amountToStake / rate;
 
         id2index[_id] = dappIdx;
 
@@ -60,21 +61,9 @@ contract DAppStore {
     
     // -- TEST
     function numVotesToMint(Dapp storage d, uint256 _SNTBalance) internal view returns(uint256) {  
-        uint TOTAL_SNT = d.SNTBalance;
-        uint SNT_PROPORTION = TOTAL_SNT * percentage_num;
-        assert(SNT_PROPORTION / TOTAL_SNT == percentage_num);
-        SNT_PROPORTION /= percentage_den;
-
-        uint num_votes_to_mint_at_1 =  percentage_den / percentage_num;
-
-        if (_SNTBalance <= SNT_PROPORTION) {
-            return num_votes_to_mint_at_1; 
-        }
-
-        if (_SNTBalance > SNT_PROPORTION) {
-            uint current_interval_index = _SNTBalance / SNT_PROPORTION;         
-            return num_votes_to_mint_at_1 + ((current_interval_index * (((_SNTBalance/100) - 1) / d.effectiveBalance)) * num_votes_to_mint_at_1);
-        }
+        uint256 cost = costOfMinting(d.id, _SNT);
+        uint256 num_to_mint = 1 / cost;
+        return num_to_mint;
     } 
 
     // -- TEST
@@ -82,7 +71,16 @@ contract DAppStore {
         uint dappIdx = id2index[_id];
         Dapp storage d = dapps[dappIdx];
         require(d.id == _id);
-        return numVotesToMint(d, _SNT);
+        
+        uint256 prev_minted = d.prevMinted;
+        uint256 num_to_mint_1 = prev_minted + ((d.SNTBalance * prev_minted)/(d.SNTBalance + _SNT))**(curve);
+        uint256 num_to_mint_percent_avail = curve * num_to_mint_1;
+        uint256 percent_votes = (d.effectiveBalance / d.SNTBalance) * 100;
+        uint256 votes_per_snt = percent_votes * num_to_mint_percent_avail;
+        uint256 num_to_mint = num_to_mint_percent_avail - votes_per_snt;
+        uint256 cost = 1 / num_to_mint;
+
+        return cost;
     }
     
     // -- TEST
@@ -96,6 +94,8 @@ contract DAppStore {
         require(SNT.allowance(msg.sender, address(this)) >= _amountToStake);
         require(SNT.transferFrom(msg.sender, address(this), _amountToStake));
         
+        uint newMinted = d.prevMinted + ((d.SNTBalance * d.prevMinted)/(d.SNTBalance + _amountToStake))**(curve);
+        d.prevMinted == newMinted;
         d.SNTBalance += _amountToStake;
 
         emit IncreasedDAppStake(_id, _amountToStake);
@@ -125,15 +125,6 @@ contract DAppStore {
         uint dappvotes = numVotesToMint(d, _amount);
         mint(d, dappvotes, false);
 
-        uint negative_votes_before = d.effectiveBalance;
-        uint negative_votes_now = d.effectiveBalance + dappvotes;
-        uint negative_percent = ((negative_votes_now - negative_votes_before) * 100 / negative_votes_now );
-        d.effectiveBalance -=   d.effectiveBalance * negative_percent / 100;
-
-        assert(d.effectiveBalance < negative_votes_before);
-
-        // TODO: what happens if effectiveValance reaches 0;
-
         require(SNT.allowance(msg.sender, d.developer) >= _amount);
         require(SNT.transferFrom(msg.sender, d.developer, _amount));
     }
@@ -148,7 +139,13 @@ contract DAppStore {
         require(_amount != 0);
         require(d.SNTBalance >= _amount);
         require(d.developer == msg.sender);
-
+        
+        // TODO: this is the biggest problem with tracking prevMinted - not sure how to handle it when
+        // the developer withdraws stake. What happens if more votes per SNT havebeen cast than votes 
+        // are available after the withdrawal, for instance?
+        uint newMinted = d.prevMinted - ((d.SNTBalance * d.prevMinted)/(d.SNTBalance + _amount))**(curve);
+        d.prevMinted == newMinted;
+        
         d.SNTBalance -= _amount; // TODO: what happens if balance is 0? Dapp is deleted?
         
         require(SNT.transferFrom(address(this), msg.sender, _amount));
