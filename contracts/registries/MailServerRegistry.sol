f44abdc7bedc89553fa3ec3d7cb3bfb207add9c4
Richard Ramos <info@richardramos.me>
2018-07-25 06:56:38 -0400

    Create MailServerRegistry.sol

---
+++ contracts/registries/MailServerRegistry.sol
@@ -0,0 +1,37 @@
+pragma solidity ^0.4.23;
+
+import "../common/Controlled.sol";
+
+
+interface IRegistry {
+    
+        function add(bytes a) external;
+        
+        function remove(bytes a) external;
+        
+        function exists(bytes a) external view returns (bool);
+}
+
+
+contract MailServerRegistry is IRegistry, Controlled {
+    
+    event MailServerAdded(bytes a);
+    event MailServerRemoved(bytes a);
+    
+    mapping(bytes => bool) servers;
+    
+    function add(bytes a) external onlyController {
+        servers[a] = true;
+        emit MailServerAdded(a);
+    }
+    
+    function remove(bytes a) external onlyController {
+        delete servers[a];
+        emit MailServerRemoved(a);
+    }
+    
+    function exists(bytes a) external view returns (bool) {
+        return servers[a];
+    }
+    
+}
