451b8c2995b5a92410f40f31830f933ecf7e272c
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2018-09-05 03:48:25 -0300

    small fixes

--- test/ensregistry.js
+++ test/ensregistry.js
@@ -1,33 +1,35 @@
 const utils = require('../utils/testUtils.js');
+const ENSRegistry = require('Embark/contracts/ENSRegistry');
 const web3Utils = require('web3-utils');
 const namehash = require('eth-ens-namehash');
 
+config({
+    contracts: {
+        "ENSRegistry": { },
+    }
+});
+
 contract('ENS', function () {
     this.timeout(0);
-    let ens;
     let accountsArr;
 
     before(function(done) {
-        var contractsConfig = {
-          "ENSRegistry": { },
-        };
-        config(contractsConfig, function(accounts) { 
-          ens = ENSRegistry;
-          accountsArr = accounts; 
-          done()
+        web3.eth.getAccounts().then(function (res) {
+            accountsArr = res;
+            done();
         });
-      });
+    });
 
     it('should allow ownership transfers', async () => {
-        let result = await ens.methods.setOwner(utils.zeroBytes32, accountsArr[1]).send({from: accountsArr[0]});       
-        assert.equal(await ens.methods.owner(utils.zeroBytes32).call(), accountsArr[1])
+        let result = await ENSRegistry.methods.setOwner(utils.zeroBytes32, accountsArr[1]).send({from: accountsArr[0]});       
+        assert.equal(await ENSRegistry.methods.owner(utils.zeroBytes32).call(), accountsArr[1])
         assert.equal(result.events.Transfer.returnValues.node, utils.zeroBytes32);
         assert.equal(result.events.Transfer.returnValues.owner, accountsArr[1]);
     });
 
     it('should prohibit transfers by non-owners', async () => {
         try {
-            await ens.methods.setOwner(utils.zeroBytes32, accountsArr[2]).send({from: accountsArr[0]});
+            await ENSRegistry.methods.setOwner(utils.zeroBytes32, accountsArr[2]).send({from: accountsArr[0]});
         } catch (error) {
             return utils.ensureException(error);
         }
@@ -36,16 +38,16 @@ contract('ENS', function () {
     });
 
     it('should allow setting resolvers', async () => {
-        let result = await ens.methods.setResolver(utils.zeroBytes32, accountsArr[3]).send({from: accountsArr[1]});        
+        let result = await ENSRegistry.methods.setResolver(utils.zeroBytes32, accountsArr[3]).send({from: accountsArr[1]});        
         let args = result.events.NewResolver.returnValues;
         assert.equal(args.node, utils.zeroBytes32);
         assert.equal(args.resolver, accountsArr[3]);
-        assert.equal(await ens.methods.resolver(utils.zeroBytes32).call(), accountsArr[3]);
+        assert.equal(await ENSRegistry.methods.resolver(utils.zeroBytes32).call(), accountsArr[3]);
     });
 
     it('should prevent setting resolvers by non-owners', async () => {
         try {
-            await ens.methods.setResolver(utils.zeroBytes32, accountsArr[4]).send({from: accountsArr[0]});
+            await ENSRegistry.methods.setResolver(utils.zeroBytes32, accountsArr[4]).send({from: accountsArr[0]});
         } catch (error) {
             return utils.ensureException(error);
         }
@@ -54,8 +56,8 @@ contract('ENS', function () {
     });
 
     it('should allow setting the TTL', async () => {
-        let result = await ens.methods.setTTL(utils.zeroBytes32, 3600).send({from: accountsArr[1]});
-        assert.equal(await ens.methods.ttl(utils.zeroBytes32).call(), 3600);
+        let result = await ENSRegistry.methods.setTTL(utils.zeroBytes32, 3600).send({from: accountsArr[1]});
+        assert.equal(await ENSRegistry.methods.ttl(utils.zeroBytes32).call(), 3600);
         let args = result.events.NewTTL.returnValues;
         assert.equal(args.node, utils.zeroBytes32);
         assert.equal(args.ttl, 3600);
@@ -63,7 +65,7 @@ contract('ENS', function () {
 
     it('should prevent setting the TTL by non-owners', async () => {
         try {
-            await ens.methods.setTTL(utils.zeroBytes32, 1200).send({from: accountsArr[0]});
+            await ENSRegistry.methods.setTTL(utils.zeroBytes32, 1200).send({from: accountsArr[0]});
         } catch (error) {
             return utils.ensureException(error);
         }
@@ -72,8 +74,8 @@ contract('ENS', function () {
     });
 
     it('should allow the creation of subnodes', async () => {
-        let result = await ens.methods.setSubnodeOwner(utils.zeroBytes32, web3Utils.sha3('eth'), accountsArr[2]).send({from: accountsArr[1]});
-        assert.equal(await ens.methods.owner(namehash.hash('eth')).call(), accountsArr[2]);
+        let result = await ENSRegistry.methods.setSubnodeOwner(utils.zeroBytes32, web3Utils.sha3('eth'), accountsArr[2]).send({from: accountsArr[1]});
+        assert.equal(await ENSRegistry.methods.owner(namehash.hash('eth')).call(), accountsArr[2]);
         let args = result.events.NewOwner.returnValues;
         assert.equal(args.node, utils.zeroBytes32);
         assert.equal(args.label, web3Utils.sha3('eth'));
@@ -82,7 +84,7 @@ contract('ENS', function () {
 
     it('should prohibit subnode creation by non-owners', async () => {
         try {
-            await ens.methods.setSubnodeOwner(utils.zeroBytes32, web3Utils.sha3('eth'), accountsArr[3]).send({from: accountsArr[0]});
+            await ENSRegistry.methods.setSubnodeOwner(utils.zeroBytes32, web3Utils.sha3('eth'), accountsArr[3]).send({from: accountsArr[0]});
         } catch (error) {
             return utils.ensureException(error);
         }
