d93457b5b4f574ae7164ba204915bb6701f60b38
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-01-19 15:50:00 -0200

    add scarcity to tests

--- test/stickermarket.js
+++ test/stickermarket.js
@@ -27,41 +27,72 @@ contract("StickerMarket", function() {
     this.timeout(0);
     var accounts;
 
+    const testPack = [
+        "QmU7VWfd3DN1Hh8fjALhQyJLgtkwxkYP2zz9MDT4rkyVJ1",
+        "QmdhTuX4V4uAUKotFTFpeHDEkSDvWVGfsvqT5EwtmtnPUW",
+        "Qmep61aZqJhhmSkhQHUSUme5RFbi8ZfccxXC1TyjKHcEig",
+        "QmVFnUPr8M49AHqprnK5ca5LdE2tJFmStJ1id9BBxHdAof",
+        "QmTbhNNgnSzDnQj8mLELcxqZKwUwbzpnHj2iMeqscjpDEF",
+        "QmVBJ5meMDhEUBqXygQdm56Gwq9ycTfiyznN2gc516pp3o",
+        "QmazRLjjzLYDnxVMEMEmKd5w8KxYMyaQ3wMakLcz5iKyNc",
+        "QmVjPM3duQJvu56PeiRy5iv7uNGjm9yxe5MTBDrpd2je25"
+    ]
+    const testPackMerkleTree = new MerkleTree(testPack);
+    const testPackMerkleRoot = testPackMerkleTree.getHexRoot();
+    const testPackPrice = "10000000000000000000";
+    var packOwner;
+    var packId;
+    var packBuyer;
 
     before(function(done) {
         web3.eth.getAccounts().then(function (res) {
             accounts = res;
+            packOwner = accounts[1]
+            packBuyer = accounts[2]
             done();
         });
     });
 
-    it("should register pack", async function() {
-        let testPack = [
-            "QmU7VWfd3DN1Hh8fjALhQyJLgtkwxkYP2zz9MDT4rkyVJ1",
-            "QmdhTuX4V4uAUKotFTFpeHDEkSDvWVGfsvqT5EwtmtnPUW",
-            "Qmep61aZqJhhmSkhQHUSUme5RFbi8ZfccxXC1TyjKHcEig",
-            "QmVFnUPr8M49AHqprnK5ca5LdE2tJFmStJ1id9BBxHdAof",
-            "QmTbhNNgnSzDnQj8mLELcxqZKwUwbzpnHj2iMeqscjpDEF",
-            "QmVBJ5meMDhEUBqXygQdm56Gwq9ycTfiyznN2gc516pp3o",
-            "QmazRLjjzLYDnxVMEMEmKd5w8KxYMyaQ3wMakLcz5iKyNc",
-            "QmVjPM3duQJvu56PeiRy5iv7uNGjm9yxe5MTBDrpd2je25"
-        ]
-        let testPackMerkleTree = new MerkleTree(testPack);
-        let testPackMerkleRoot = testPackMerkleTree.getHexRoot();
-        let testPackPrice = "10000000000000000000";
-        let packOwner = accounts[1];
-        await StickerMarket.methods.register(testPackMerkleRoot, testPackPrice, packOwner, "0x").send();
-        
+    it("should register unlimited pack", async function() {
+
+        await StickerMarket.methods.register(testPackMerkleRoot, testPackPrice, 0, packOwner, "0x").send();
+        packId = "0";
     });
 
-    it("should buy a pack", async function() {
-        let packBuyer = accounts[2];
-        let packId = "0";
+    it("should buy unlimited pack", async function() {
         let packPrice = await StickerMarket.methods.priceOf(packId).call();
         await TestStatusNetwork.methods.mint(packPrice).send({from: packBuyer });
-
         await MiniMeToken.methods.approve(StickerMarket.address, packPrice).send({from: packBuyer });
         await StickerMarket.methods.buy(packId).send({from: packBuyer });
+
+    });
+
+    it("should buy another unlimited pack", async function() {
+        let packPrice = await StickerMarket.methods.priceOf(packId).call();
+        await TestStatusNetwork.methods.mint(packPrice).send({from: packBuyer });
+        await MiniMeToken.methods.approve(StickerMarket.address, packPrice).send({from: packBuyer });
+        await StickerMarket.methods.buy(packId).send({from: packBuyer });
+
+    });
+
+    it("should register limited pack", async function() {
+        await StickerMarket.methods.register("0xDEADBEEF", testPackPrice, 1, packOwner, "0x").send();
+        packId = "1";        
+    });
+
+    it("should buy a limited pack", async function() {
+        let packPrice = await StickerMarket.methods.priceOf(packId).call();
+        await TestStatusNetwork.methods.mint(packPrice).send({from: packBuyer });
+        await MiniMeToken.methods.approve(StickerMarket.address, packPrice).send({from: packBuyer });
+        await StickerMarket.methods.buy(packId).send({from: packBuyer });
+
+    });
+
+    it("should not buy another limited pack", async function() {
+        let packPrice = await StickerMarket.methods.priceOf(packId).call();
+        await TestStatusNetwork.methods.mint(packPrice).send({from: packBuyer });
+        await MiniMeToken.methods.approve(StickerMarket.address, packPrice).send({from: packBuyer });
+        Utils.expectThrow(StickerMarket.methods.buy(packId).send({from: packBuyer }))
 
     });
 
