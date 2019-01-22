404d68c998a45d0a2030b01633108c70eabf18fa
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-01-22 03:18:55 -0200

    change to monocontract

--- contracts/status/sticker-market/StickerMarket.sol
+++ contracts/status/sticker-market/StickerMarket.sol
@@ -6,11 +6,11 @@ import "../../token/ApproveAndCallFallBack.sol";
 import "../../common/Controlled.sol";
 
 /**
- * @dev 
+ * 
  */
-contract StickerMarket is Controlled, ApproveAndCallFallBack {
-    event Register(uint256 indexed packId, bytes32 indexed stickersMerkleRoot, uint256 dataPrice, bytes _contenthash);
-    event Unregister(uint256 indexed packId, bytes32 indexed stickersMerkleRoot);
+contract StickerMarket is Controlled, StickerPack, ApproveAndCallFallBack {
+    event Register(uint256 indexed marketId, bytes32 indexed stickersMerkleRoot, uint256 dataPrice, bytes _contenthash);
+    event Unregister(uint256 indexed marketId, bytes32 indexed stickersMerkleRoot);
     event ClaimedTokens(address indexed _token, address indexed _controller, uint256 _amount);
 
     struct Pack {
@@ -18,16 +18,23 @@ contract StickerMarket is Controlled, ApproveAndCallFallBack {
         uint256 price;
         address owner;
     }
-    StickerPack public stickerPack = new StickerPack(); 
     ERC20Token public snt;    
-    mapping(uint256 => Pack) public packs;
-    mapping(bytes32 => uint256) public packIds;
+    bool public marketEnabled;
+    mapping(uint256 => Pack) public marketPacks;
+    mapping(bytes32 => uint256) public marketIds;
     mapping(bytes32 => bytes) public packContenthash;
 
-    uint256 public nextId;
+    uint256 public marketCount;
+
+    uint256[] public availablePacks;
+    mapping (uint256 => uint256) private _availablePacksPos;
+
+    modifier market {
+        require(marketEnabled, "Market Disabled");
+        _;
+    }
 
     /**
-     * @notice Constructor
      * @param _snt SNT token
      */
     constructor(
@@ -38,44 +45,42 @@ contract StickerMarket is Controlled, ApproveAndCallFallBack {
         snt = _snt;
     }
 
-    function buy(uint256 _packId) external returns (uint256 tokenId) {
-        return _buy(msg.sender, packs[_packId]);
+    function buyStickerPackToken(uint256 _marketId) external market returns (uint256 tokenId) {
+        return _buy(msg.sender, marketPacks[_marketId]);
     }
     
-    function receiveApproval(address _from, uint256 _amount, address _token, bytes calldata _data) external {
+    function receiveApproval(address _from, uint256 _amount, address _token, bytes calldata _data) external market {
         require(_token == address(snt), "Bad token");
         require(_token == address(msg.sender), "Bad call");
         require(_data.length == 36, "Bad data length");
-        uint256 packId = abiDecodeBuy(_data);
-        Pack memory pack = packs[packId];
+        uint256 packId = abiDecodeBuyStickerPackToken(_data);
+        Pack memory pack = marketPacks[packId];
         require(pack.price == _amount, "Bad amount");
         _buy(_from, pack);
     }
 
-    function transfer(uint256 _packId, address _to) external {
-        require(packs[_packId].owner == msg.sender);
-        packs[_packId].owner = _to;
+    function transferMarketPack(uint256 _marketId, address _to) external market {
+        require(marketPacks[_marketId].owner == msg.sender);
+        marketPacks[_marketId].owner = _to;
     }
 
-    function register(bytes32 _stickersMerkleRoot, uint256 _price, address _owner, bytes calldata _packContenthash) external onlyController {
-        require(packs[packIds[_stickersMerkleRoot]].stickersMerkleRoot != _stickersMerkleRoot, "Duplicated");
-        uint256 packId = nextId++;
-        packs[packId] = Pack(_stickersMerkleRoot, _price, _owner);
-        packIds[_stickersMerkleRoot] = packId;
+    function registerMarketPack(bytes32 _stickersMerkleRoot, uint256 _price, address _owner, bytes calldata _packContenthash) external onlyController {
+        require(marketPacks[marketIds[_stickersMerkleRoot]].stickersMerkleRoot != _stickersMerkleRoot, "Duplicated");
+        uint256 marketId = marketCount++;
+        marketPacks[marketId] = Pack(_stickersMerkleRoot, _price, _owner);
+        marketIds[_stickersMerkleRoot] = marketId;
         packContenthash[_stickersMerkleRoot] = _packContenthash;
-        emit Register(packId, _stickersMerkleRoot, _price, _packContenthash);
+        addAvailablePack(marketId);
+        emit Register(marketId, _stickersMerkleRoot, _price, _packContenthash);
     }
 
 
-    function unregister(uint256 _packId) external onlyController {
-        bytes32 stickersMerkleRoot = packs[_packId].stickersMerkleRoot;
-        delete packIds[stickersMerkleRoot];
-        delete packs[_packId];
-        emit Unregister(_packId, stickersMerkleRoot);
-    }
-
-    function migrateMarket(address payable _newMarket) external onlyController {
-        stickerPack.changeController(_newMarket);
+    function unregisterMarketPack(uint256 _marketId) external onlyController {
+        bytes32 stickersMerkleRoot = marketPacks[_marketId].stickersMerkleRoot;
+        delete marketIds[stickersMerkleRoot];
+        delete marketPacks[_marketId];
+        removeAvailablePack(_marketId);
+        emit Unregister(_marketId, stickersMerkleRoot);
     }
 
     /**
@@ -95,30 +100,30 @@ contract StickerMarket is Controlled, ApproveAndCallFallBack {
         emit ClaimedTokens(_token, controller, balance);
     }
 
-    function priceOf(uint256 _packId) external view returns(uint256 price){
-        price = packs[_packId].price;
+    function marketPriceOf(uint256 _marketId) external view returns(uint256 price){
+        price = marketPacks[_marketId].price;
     }
     
-    function ownerOf(uint256 _packId) external view returns(address owner){
-        owner = packs[_packId].owner;
+    function marketPackOwner(uint256 _marketId) external view returns(address owner){
+        owner = marketPacks[_marketId].owner;
     }
 
-    function dataOf(uint256 _packId) external view returns(bytes32 stickersMerkleRoot){
-        stickersMerkleRoot = packs[_packId].stickersMerkleRoot;
+    function marketPackMerkleRoot(uint256 _marketId) external view returns(bytes32 stickersMerkleRoot){
+        stickersMerkleRoot = marketPacks[_marketId].stickersMerkleRoot;
     }
 
     function _buy(address _buyer, Pack memory _pack) internal returns (uint256 tokenId){
         require(_pack.stickersMerkleRoot != bytes32(0), "Bad pack");
         require(snt.transferFrom(_buyer, _pack.owner, _pack.price), "Bad payment");
-        return stickerPack.generateToken(_buyer, _pack.stickersMerkleRoot);
+        return generateToken(_buyer, _pack.stickersMerkleRoot);
     }
 
     /**
-     * @dev Decodes abi encoded data with selector for "buy(uint256)".
+     * @dev Decodes abi encoded data with selector for "buyStickerPackToken(uint256)".
      * @param _data Abi encoded data.
      * @return Decoded registry call.
      */
-    function abiDecodeBuy(
+    function abiDecodeBuyStickerPackToken(
         bytes memory _data
     ) 
         private 
@@ -132,6 +137,18 @@ contract StickerMarket is Controlled, ApproveAndCallFallBack {
             sig := mload(add(_data, add(0x20, 0)))
             packId := mload(add(_data, 36))
         }
-        require(sig == bytes4(keccak256("buy(uint256)")), "Bad method sig");
+        require(sig == bytes4(keccak256("buyStickerPackToken(uint256)")), "Bad method sig");
+    }
+
+    function addAvailablePack(uint256 _marketId) internal {
+        _availablePacksPos[_marketId] = availablePacks.push(_marketId);
+    }
+    
+    function removeAvailablePack(uint256 _marketId) internal {
+        uint pos = _availablePacksPos[_marketId];
+        uint256 movedElement = availablePacks[availablePacks.length-1]; //tokenId;
+        availablePacks[pos] = movedElement;
+        availablePacks.length--;
+        _availablePacksPos[movedElement] = pos;
     }
 }
--- contracts/status/sticker-market/StickerPack.sol
+++ contracts/status/sticker-market/StickerPack.sol
@@ -10,7 +10,7 @@ contract StickerPack is Controlled, UnfungibleToken {
     uint256 public nextId;
     mapping(uint256 => bytes32) public dataHash; 
 
-    function generateToken(address _owner, bytes32 _dataHash) external onlyController returns (uint256 tokenId){
+    function generateToken(address _owner, bytes32 _dataHash) public onlyController returns (uint256 tokenId){
         tokenId = nextId++;
         dataHash[tokenId] = _dataHash;
         mint(_owner, tokenId);
