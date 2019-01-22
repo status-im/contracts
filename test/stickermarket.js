cea9dd074c24abc12435ef80fbbe5c3bdf8f6330
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-01-22 04:50:45 -0200

    update tests

--- test/stickermarket.js
+++ test/stickermarket.js
@@ -19,7 +19,7 @@ config({
     },
     "StickerMarket": {
         "args": ["$MiniMeToken"]
-    },
+    }
   }
 });
 
@@ -36,6 +36,7 @@ contract("StickerMarket", function() {
     });
 
     it("should register pack", async function() {
+        await StickerMarket.methods.setMarketState(true).send();
         let testPack = [
             "QmU7VWfd3DN1Hh8fjALhQyJLgtkwxkYP2zz9MDT4rkyVJ1",
             "QmdhTuX4V4uAUKotFTFpeHDEkSDvWVGfsvqT5EwtmtnPUW",
@@ -50,18 +51,18 @@ contract("StickerMarket", function() {
         let testPackMerkleRoot = testPackMerkleTree.getHexRoot();
         let testPackPrice = "10000000000000000000";
         let packOwner = accounts[1];
-        await StickerMarket.methods.register(testPackMerkleRoot, testPackPrice, packOwner, "0x").send();
+        await StickerMarket.methods.registerMarketPack(testPackMerkleRoot, testPackPrice, packOwner, "0x").send();
         
     });
 
-    it("should buy a pack", async function() {
+    it("should buy a pack when market enabled", async function() {
+        
         let packBuyer = accounts[2];
         let packId = "0";
-        let packPrice = await StickerMarket.methods.priceOf(packId).call();
+        let packPrice = await StickerMarket.methods.marketPriceOf(packId).call();
         await TestStatusNetwork.methods.mint(packPrice).send({from: packBuyer });
-
         await MiniMeToken.methods.approve(StickerMarket.address, packPrice).send({from: packBuyer });
-        await StickerMarket.methods.buy(packId).send({from: packBuyer });
+        await StickerMarket.methods.buyStickerPackToken(packId).send({from: packBuyer });
 
     });
 
