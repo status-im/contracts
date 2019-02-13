ee85e58d8c239d81f4482afab4fc87abe5c97224
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-02-13 05:10:20 -0200

    add events, rename, views

--- contracts/status/Ranking.sol
+++ contracts/status/Ranking.sol
@@ -1,48 +1,55 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-/**
- * @notice Linked list which requires objects being linked in value descending order, from head to tai* @author Ricardo Guilherme Schmidt
- */
-contract Ranking {
+import "../common/Controlled.sol";
 
-    struct Object {
+/**
+ * @notice Linked list which requires entries being linked in points descending order, from head to tai* @author Ricardo Guilherme Schmidt
+ */
+contract Ranking is Controlled {
+    event NewEntry(bytes32 indexed uid, address indexed owner);
+    event PointsUpdate(bytes32 indexed uid, uint256 newValue);
+    struct Entry {
         uint256 timestamp;
         address owner;
-        uint256 value;
+        uint256 points;
         bytes32 next;
         //bytes32 previous;
     }
 
-    bytes32 top;
-    mapping (bytes32 => Object) object;
+    bytes32 public top;
+    mapping (bytes32 => Entry) public entries;
 
-    function include(bytes32 _uid, uint256 _value, bytes32 _before) external {
-        require(object[_uid].timestamp == uint256(0), "Duplicate");
+    function include(bytes32 _uid, address _owner, uint256 _points, bytes32 _before) external onlyController {
+        require(entries[_uid].timestamp == uint256(0), "Duplicate");
         bytes32 next;
         if(_before == bytes32(0)){
             if(top != bytes32(0)){
-                require(object[top].value >= _value, "Not top position");
+                require(entries[top].points >= _points, "Not top position");
                 next = top;
             }
             top = _uid;
         } else {
-            require(object[_before].value >= _value, "Bad position");
-            next = object[_before].next;
-            require(object[next].value <= _value, "Bad position 2");
-            object[_before].next = _uid;
+            require(entries[_before].points >= _points, "Bad position");
+            next = entries[_before].next;
+            require(entries[next].points <= _points, "Bad position 2");
+            entries[_before].next = _uid;
         }
-        object[_uid] = Object(block.timestamp, msg.sender, _value, next);
+        entries[_uid] = Entry(block.timestamp, _owner, _points, next);
+        emit NewEntry(_uid, _owner);
+        emit PointsUpdate(_uid, _points);
     }
 
-    function increase(bytes32 _uid, bytes32 _oldPrevious, bytes32 _newPrevious, uint256 _value) external {
-        uint256 oldValue = object[_uid].value;
-        move(_uid, _oldPrevious, _newPrevious, oldValue + _value);
+    function increase(bytes32 _uid, uint256 _points, bytes32 _oldPrevious, bytes32 _newPrevious) external onlyController {
+        require(entries[_uid].timestamp != uint256(0), "Unknown entry");
+        uint256 oldValue = entries[_uid].points;
+        move(_uid, _oldPrevious, _newPrevious, oldValue + _points);
     }
 
-    function decline(bytes32 _uid, bytes32 _oldPrevious, bytes32 _newPrevious, uint256 _value) external {
-        uint256 oldValue = object[_uid].value;
-        require(oldValue >= _value,"Bad value");
-        uint256 newValue = oldValue - _value;
+    function decrease(bytes32 _uid, uint256 _points, bytes32 _oldPrevious, bytes32 _newPrevious) external onlyController {
+        require(entries[_uid].timestamp != uint256(0), "Unknown entry");
+        uint256 oldValue = entries[_uid].points;
+        require(oldValue >= _points, "Bad points");
+        uint256 newValue = oldValue - _points;
         if(newValue > 0) {
             move(_uid, _oldPrevious, _newPrevious, newValue);
         } else {
@@ -52,12 +59,32 @@ contract Ranking {
         
     }
 
+    function exists(bytes32 _uid) external view returns (bool) {
+        return entries[_uid].timestamp != uint256(0);
+    }
+
+    function pointsOf(bytes32 _uid) external view returns (uint256) {
+        return entries[_uid].points;
+    }
+    
+    function ownerOf(bytes32 _uid) external view returns (address) {
+        return entries[_uid].owner;
+    }
+
+    function nextOf(bytes32 _uid) external view returns (bytes32) {
+        return entries[_uid].next;
+    }
+    
+    function timestampOf(bytes32 _uid) external view returns (uint256) {
+        return entries[_uid].timestamp;
+    }
+
     function move(bytes32 _uid, bytes32 _oldPrevious, bytes32 _newPrevious, uint256 _newValue) private {
         bytes32 newNext;
-        Object storage moving = object[_uid];
+        Entry storage moving = entries[_uid];
         require(moving.timestamp != 0, "Unknown uid");
         if(_oldPrevious != bytes32(0)){
-            Object storage oldBefore = object[_oldPrevious];
+            Entry storage oldBefore = entries[_oldPrevious];
             require(oldBefore.timestamp != 0, "Unknown oldPrevious");
             require(oldBefore.next == _uid, "Invalid oldPrevious");
             oldBefore.next = moving.next;
@@ -66,21 +93,21 @@ contract Ranking {
         }
          
         if(_newPrevious != bytes32(0)){
-            Object storage newBefore = object[_newPrevious];
+            Entry storage newBefore = entries[_newPrevious];
             require(newBefore.timestamp != 0, "Unknown newPrevious");
             newNext = newBefore.next;
-            Object memory newNextObj = object[newBefore.next];
+            Entry memory newNextObj = entries[newBefore.next];
 
-            if(newBefore.value == _newValue){
-                require(newBefore.timestamp < moving.timestamp, "Low newPrevious value");
+            if(newBefore.points == _newValue){
+                require(newBefore.timestamp < moving.timestamp, "Low newPrevious points");
             } else {
-                require(newBefore.value >= _newValue, "Low newPrevious value");
+                require(newBefore.points >= _newValue, "Low newPrevious points");
             }
             if(newNext != bytes32(0)){
-                if(newNextObj.value == _newValue){
-                    require(newNextObj.timestamp > moving.timestamp, "High newNext value");
+                if(newNextObj.points == _newValue){
+                    require(newNextObj.timestamp > moving.timestamp, "High newNext points");
                 } else {
-                    require(newNextObj.value < _newValue, "High newNext value");
+                    require(newNextObj.points < _newValue, "High newNext points");
                 }
             }
             newBefore.next = _uid;
@@ -89,15 +116,15 @@ contract Ranking {
             top = _uid;
         }
         moving.next = newNext;
-        moving.value = _newValue;
-        
+        moving.points = _newValue;
+        emit PointsUpdate(_uid, _newValue);
     }
 
     function exclude(bytes32 _uid, bytes32 _oldPrevious) private {
-        Object memory excluded = object[_uid];
+        Entry memory excluded = entries[_uid];
         require(excluded.timestamp != 0, "Unknown uid");
         if(_oldPrevious != bytes32(0)){
-            Object storage oldBefore = object[_oldPrevious];
+            Entry storage oldBefore = entries[_oldPrevious];
             require(oldBefore.timestamp != 0, "Unknown oldPrevious");
             require(oldBefore.next == _uid, "Invalid oldPrevious");
             oldBefore.next = excluded.next;
@@ -105,7 +132,7 @@ contract Ranking {
             require(top == _uid, "Bad argument");
             top = excluded.next;
         }
-        delete object[_uid];
+        delete entries[_uid];
 
     }
 }
