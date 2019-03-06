26a1e4cbf0bd75987e0a086bfe4a17d734376389
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-03-06 01:38:05 -0300

    add test spec

---
+++ test/ranking.js
@@ -0,0 +1,577 @@
+const utils = require('../utils/testUtils');
+const Ranking = require('Embark/contracts/Ranking');
+
+config({
+    contracts: {
+        "Ranking": {
+            "args":["$MiniMeTokenFactory", "0x0", "0x0", "Status Test Token", 18, "STT", true],
+        }
+    }
+});
+
+const item_0a = "0x000000000000000000000000000000000000000000000000000000000000000a"
+const item_0b = "0x000000000000000000000000000000000000000000000000000000000000000b"
+const item_0c = "0x000000000000000000000000000000000000000000000000000000000000000c"
+const item_0d = "0x000000000000000000000000000000000000000000000000000000000000000d"
+const item_0e = "0x000000000000000000000000000000000000000000000000000000000000000e"
+const item_0f = "0x000000000000000000000000000000000000000000000000000000000000000f"
+const item_aa = "0x00000000000000000000000000000000000000000000000000000000000000aa"
+const item_bb = "0x00000000000000000000000000000000000000000000000000000000000000bb"
+const item_cc = "0x00000000000000000000000000000000000000000000000000000000000000cc"
+const item_dd = "0x00000000000000000000000000000000000000000000000000000000000000dd"
+const item_ee = "0x00000000000000000000000000000000000000000000000000000000000000ee"
+const item_ff = "0x00000000000000000000000000000000000000000000000000000000000000ff"
+
+async function printRanking(Ranking) {
+    var next = await Ranking.methods.top().call();
+    var i = 0;
+    console.log("Ranking: ")
+    while(next != utils.zeroBytes32){
+        console.log(
+            i++,
+            "current " + (next).substring(0,8), 
+            await Ranking.methods.pointsOf(next).call(),
+            "previous " + (await Ranking.methods.previousOf(next).call()).substring(0,8),
+            "next " + (await Ranking.methods.nextOf(next).call()).substring(0,8),  
+        );
+        next = await Ranking.methods.nextOf(next).call();
+    }
+}
+
+contract("Ranking", function() {
+    this.timeout(0);
+    var accounts;
+    before(function(done) {
+        web3.eth.getAccounts().then(function (res) {
+            accounts = res;
+            done();
+        });
+    });
+
+    it("should begin with empty top", async function() {
+        let top = await Ranking.methods.top().call();
+        assert.equal(top, utils.zeroBytes32);
+    });
+
+    it("should not include in unexistent element while top empty", async function() {
+        try{
+            await Ranking.methods.include(
+                item_0a,
+                accounts[1],
+                100000000,
+                item_0f
+                ).send({from: accounts[0]});    
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('did not fail'); 
+    });
+
+    it("should include first as top", async function() {
+        await Ranking.methods.include(
+            item_0a,
+            accounts[1],
+            100000000,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});
+        assert.equal(await Ranking.methods.top().call(), item_0a, "top is item 0a")
+        assert.equal(await Ranking.methods.previousOf(item_0a).call(), utils.zeroBytes32, "item 0a previous is null (head)")
+        assert.equal(await Ranking.methods.nextOf(item_0a).call(), utils.zeroBytes32, "item 0a next is null (tail)")
+    })
+
+    it("should not include as top if new not top", async function() {
+        try{
+            await Ranking.methods.include(
+                item_0b,
+                accounts[1],
+                99999990,
+                utils.zeroBytes32
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        
+        assert.fail('did not fail');
+    })
+
+    it("should not include with previous to non existent element", async function() {
+        try{
+            await Ranking.methods.include(
+                item_0b,
+                accounts[1],
+                99999990,
+                item_0c,
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('did not fail');
+    })
+
+    it("should second include with previous as top", async function() {
+        await Ranking.methods.include(
+            item_0b,
+            accounts[1],
+            99999990,
+            item_0a
+        ).send({from: accounts[0]});
+        
+        assert.equal(
+            await Ranking.methods.nextOf(item_0a).call(),
+            item_0b, 
+            "item 0a next is item 0b"
+        )   
+        assert.equal(
+            await Ranking.methods.previousOf(item_0b).call(), 
+            item_0a, 
+            "item 0b previous is item 0a"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0b).call(),
+            utils.zeroBytes32, 
+            "item 0b next is null (tail)"
+        )
+
+    })
+
+    it("should include third with second as previous", async function() {
+        await Ranking.methods.include(
+            item_0c,
+            accounts[1],
+            99999980,
+            item_0b
+        ).send({from: accounts[0]});
+        
+        assert.equal(
+            await Ranking.methods.nextOf(item_0b).call(),
+            item_0c, 
+            "item 0b next is item 0c"
+        )   
+        assert.equal(
+            await Ranking.methods.previousOf(item_0c).call(), 
+            item_0b, 
+            "item 0c previous is item 0b"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0c).call(),
+            utils.zeroBytes32, 
+            "item 0c next is null (tail)"
+        )
+    })
+    
+    it("should not include forth with second as previous", async function() {
+        try{
+            await Ranking.methods.include(
+                item_0d,
+                accounts[1],
+                99999970,
+                item_0b
+            ).send({from: accounts[0]});  
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('did not fail');
+    })
+
+    it("should not include a zero element", async function() {
+        try{
+            await Ranking.methods.include(
+                utils.zeroBytes32,
+                accounts[1],
+                99999970,
+                item_0b
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('did not fail');
+    })
+
+    it("should include a couple of objects in correct order", async function() {
+        await Ranking.methods.include(
+            item_0d,
+            accounts[1],
+            99999970,
+            item_0c
+        ).send({from: accounts[0]});
+
+        await Ranking.methods.include(
+            item_0e,
+            accounts[1],
+            99999960,
+            item_0d
+        ).send({from: accounts[0]});
+        
+        assert.equal(
+            await Ranking.methods.nextOf(item_0c).call(),
+            item_0d, 
+            "item 0c next is item 0d"
+        )   
+        assert.equal(
+            await Ranking.methods.nextOf(item_0d).call(),
+            item_0e, 
+            "item 0d next is item 0e"
+        )   
+        assert.equal(
+            await Ranking.methods.previousOf(item_0d).call(), 
+            item_0c, 
+            "item 0d previous is item 0c"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0e).call(), 
+            item_0d, 
+            "item 0e previous is item 0d"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0e).call(),
+            utils.zeroBytes32, 
+            "item 0e next is null (tail)"
+        )
+
+    })
+
+    it("should include a new position in middle", async function() {
+        await Ranking.methods.include(
+            item_0f,
+            accounts[1],
+            99999965,
+            item_0d
+        ).send({from: accounts[0]});
+    
+        assert.equal(
+            await Ranking.methods.nextOf(item_0d).call(),
+            item_0f, 
+            "item 0d next is item 0f"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0f).call(), 
+            item_0d, 
+            "item 0f previous is item 0d"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0f).call(),
+            item_0e, 
+            "item 0f next is item 0e"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0e).call(), 
+            item_0f, 
+            "item 0e previous is item 0f"
+        )
+    })
+
+    it("should not increase points and move to wrong position", async function() {
+        try{
+            await Ranking.methods.increase(
+                item_0f,
+                10,
+                item_0b
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        
+        assert.fail('did not fail');
+    })
+
+
+    it("should not increase points and move to top", async function() {
+        try{
+            await Ranking.methods.increase(
+                item_0f,
+                10,
+                utils.zeroBytes32
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        
+        assert.fail('did not fail');
+    })
+
+
+    it("should increase points and move to correct position", async function() {
+        await Ranking.methods.increase(
+            item_0f,
+            10,
+            item_0c
+        ).send({from: accounts[0]});
+        
+        assert.equal(
+            await Ranking.methods.nextOf(item_0c).call(),
+            item_0f, 
+            "item 0c next is item 0f"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0f).call(), 
+            item_0c, 
+            "item 0f previous is item 0c"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0f).call(),
+            item_0d, 
+            "item 0f next is item 0d"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0d).call(), 
+            item_0f, 
+            "item 0d previous is item 0f"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0d).call(),
+            item_0e, 
+            "item 0d next is item 0e"
+        )
+    })
+    it("should decrease points and move to correct position", async function() {
+        await Ranking.methods.decrease(
+            item_0f,
+            10,
+            item_0d
+        ).send({from: accounts[0]});             
+
+        assert.equal(
+            await Ranking.methods.nextOf(item_0d).call(),
+            item_0f, 
+            "item 0d next is item 0f"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0f).call(), 
+            item_0d, 
+            "item 0f previous is item 0d"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0f).call(),
+            item_0e, 
+            "item 0f next is item 0e"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0e).call(), 
+            item_0f, 
+            "item 0e previous is item 0f"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0c).call(),
+            item_0d, 
+            "item 0c next is item 0d"
+        )
+    })
+
+    it("should not decrease points and move to wrong position", async function() {
+        try{
+            await Ranking.methods.decrease(
+                item_0f,
+                10,
+                item_0a
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        }
+        assert.fail('did not fail');
+    })
+
+    it("should increase points and move to top", async function() {
+        await Ranking.methods.increase(
+            item_0f,
+            100,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});
+
+        assert.equal(await Ranking.methods.top().call(), item_0f, "top is item 0f")
+        assert.equal(
+            await Ranking.methods.previousOf(item_0f).call(), 
+            utils.zeroBytes32, 
+            "item 0f previous is null (top)"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0f).call(),
+            item_0a, 
+            "item 0f next is item 0a"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0a).call(), 
+            item_0f, 
+            "item 0a previous is item 0f"
+        )
+
+        assert.equal(
+            await Ranking.methods.nextOf(item_0d).call(),
+            item_0e, 
+            "item 0d next is item 0e"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0e).call(), 
+            item_0d, 
+            "item 0e previous is item 0d"
+        )
+    })
+
+    it("should decrease points and move to tail", async function() {
+        await Ranking.methods.decrease(
+            item_0f,
+            200,
+            item_0e
+        ).send({from: accounts[0]});
+
+        assert.equal(await Ranking.methods.top().call(), item_0a, "top is item 0a")
+        assert.equal(
+            await Ranking.methods.previousOf(item_0a).call(), 
+            utils.zeroBytes32, 
+            "item 0a previous is null (top)"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0f).call(),
+            "0x0000000000000000000000000000000000000000000000000000000000000000", 
+            "item 0f next is null (tail)"
+        )
+        assert.equal(
+            await Ranking.methods.previousOf(item_0f).call(), 
+            item_0e, 
+            "item 0f previous is item 0e"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0e).call(),
+            item_0f, 
+            "item 0e next is item 0f"
+        )
+    })
+    it("should decrease points and be excluded from tail of list", async function() {
+        let points = await Ranking.methods.pointsOf(item_0f).call();
+        await Ranking.methods.decrease(
+            item_0f,
+            points,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});
+        // assert item 0f is gone
+        assert.equal(
+            await Ranking.methods.nextOf(item_0e).call(),
+            utils.zeroBytes32, 
+            "item 0e next is null (tail)"
+        )
+    })
+    it("should decrease points and be excluded from middle of list", async function() {
+        let points = await Ranking.methods.pointsOf(item_0c).call();
+        await Ranking.methods.decrease(
+            item_0c,
+            points,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});
+        
+        // assert item 0c is gone
+        assert.equal(
+            await Ranking.methods.previousOf(item_0d).call(), 
+            item_0b, 
+            "item 0b is previous of item 0d"
+        )
+        assert.equal(
+            await Ranking.methods.nextOf(item_0b).call(),
+            item_0d, 
+            "item 0d is next of item 0b"
+        )
+    })
+    it("should decrease points and be excluded from top of list", async function() {
+        let points = await Ranking.methods.pointsOf(item_0a).call()
+        await Ranking.methods.decrease(
+            item_0a,
+            points,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});  
+        
+        // assert item 0a is gone
+        assert.equal(await Ranking.methods.top().call(), item_0b, "top is item 0f")
+        assert.equal(
+            await Ranking.methods.previousOf(item_0b).call(), 
+            utils.zeroBytes32, 
+            "null is previous of item 0b (head)"
+        )  
+    })
+    it("should include at top", async function() {
+        await Ranking.methods.include(
+            item_aa,
+            accounts[1],
+            200000000,
+            utils.zeroBytes32
+        ).send({from: accounts[0]});
+        assert.equal(await Ranking.methods.top().call(), item_aa, "top is item aa")
+        assert.equal(await Ranking.methods.previousOf(item_aa).call(), utils.zeroBytes32, "item aa previous is null (head)")
+        assert.equal(await Ranking.methods.nextOf(item_aa).call(), item_0b, "item aa next is item 0b")
+        assert.equal(
+            await Ranking.methods.previousOf(item_0b).call(), 
+            item_aa, 
+            "item aa is previous of item 0b"
+        )
+    })
+    it("should include a couple with same points", async function() {
+        await Ranking.methods.include(
+            item_bb,
+            accounts[1],
+            190000000,
+            item_aa
+        ).send({from: accounts[0]});
+        await utils.increaseTime(1000);
+        await Ranking.methods.include(
+            item_cc,
+            accounts[1],
+            190000000,
+            item_bb
+        ).send({from: accounts[0]});
+        await utils.increaseTime(1000);
+    })
+    it("should not include with same value in wrong position", async function() {
+        try{
+            await Ranking.methods.include(
+                item_dd,
+                accounts[1],
+                190000000,
+                item_aa
+            ).send({from: accounts[0]});
+        } catch (error) {
+            return utils.ensureException(error);
+        } 
+        assert.fail('did not fail');
+    })
+    it("should decrease to same points", async function() {
+        await Ranking.methods.include(
+            item_dd,
+            accounts[1],
+            180000000,
+            item_cc
+        ).send({from: accounts[0]});
+        await utils.increaseTime(1000);
+
+        await Ranking.methods.include(
+            item_ee,
+            accounts[1],
+            170000000,
+            item_dd
+        ).send({from: accounts[0]});
+        await utils.increaseTime(1000);
+
+        await Ranking.methods.include(
+            item_ff,
+            accounts[1],
+            160000000,
+            item_ee
+        ).send({from: accounts[0]});
+        await utils.increaseTime(1000);
+
+        await Ranking.methods.decrease(
+            item_dd,
+            20000000,
+            item_ee
+        ).send({from: accounts[0]});
+    })
+    it("should increase to same points", async function() {
+        await Ranking.methods.increase(
+            item_dd,
+            15000000,
+            item_cc
+        ).send({from: accounts[0]});
+
+        await Ranking.methods.increase(
+            item_ff,
+            15000000,
+            item_dd
+        ).send({from: accounts[0]});
+    })    
+});
