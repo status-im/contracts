3eb48a9b654d9231fab46b3b4c81da1f1c9d8c4f
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-03-20 22:04:31 -0300

    small fix

--- contracts/deploy/InstanceFactory.sol
+++ contracts/deploy/InstanceFactory.sol
@@ -1,7 +1,7 @@
-pragma solidity >=0.5.3 <0.6.0;
+pragma solidity >=0.5.0 <0.6.0;
 
-import "../deploy/Instance.sol";
-import "../deploy/PrototypeRegistry.sol";
+import "./Instance.sol";
+import "./PrototypeRegistry.sol";
 
 
 /**
