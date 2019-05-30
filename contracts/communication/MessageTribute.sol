aa2d12c253b99b2d3d335db7d37446b2458f1293
Ricardo Guilherme Schmidt <3esmit@gmail.com>
2019-05-30 12:03:09 -0300

    simplify MessageTribute

--- contracts/communication/MessageTribute.sol
+++ contracts/communication/MessageTribute.sol
@@ -1,83 +1,31 @@
 pragma solidity >=0.5.0 <0.6.0;
 
-import "../common/Controlled.sol";
-
 /**
- * @title MessageTribute
- * @author Ricardo Guilherme Schmidt (Status Research & Development GmbH)* @
- * @notice User registry of Tribute to Talk manifests
+ * @notice Defines tribute to talk
  */
-contract MessageTribute is Controlled {
-    event Manifest(address indexed user, bytes manifest);
-    event Stopped(bool stop);
+contract MessageTribute {
+    event SetTribute(address indexed account, uint256 value);
 
-    bool public stopped;
-    mapping(address => bytes) private manifests;
+    mapping(address => uint256) private tributeCatalog;
 
-    modifier notStopped {
-        require(!stopped, "Contract disabled.");
-        _;
-    }
     /**
-     * @param _defaultManifest contenthash manifest returned for unset/reset users 
+     * @notice Set tribute of account
+     * @param _value Required tribute value
      */
-    constructor(bytes memory _defaultManifest) public {
-        updateManifest(address(0), _defaultManifest);
+    function setTribute(uint256 _value) external {
+        tributeCatalog[msg.sender] = _value;
+        emit SetTribute(msg.sender, _value);
     }
 
     /**
-     * @notice Set public message
-     * @param _manifest  contenthash manifest of Tribute to Talk
+     * @notice Obtain required tribute to talk with `_of`
+     * @param _of Account to lookup
+     * @return value of tribute
      */
-    function setManifest(bytes calldata _manifest) external notStopped {
-        updateManifest(msg.sender, _manifest);
-    }
-
-    /**
-     * @notice Resets account to default manifest
-     */
-    function resetManifest() external notStopped {
-        delete manifests[msg.sender];
-        emit Manifest(msg.sender, new bytes(0));
-    }
-
-    /**
-     * @notice controller can configure default fee
-     * @param _defaultManifest contenthash manifest returned for unset/reset users 
-     */
-    function setDefaultManifest(bytes calldata _defaultManifest) external onlyController {
-        updateManifest(address(0), _defaultManifest);
-    }
-
-    /**
-     * @notice controller can stop the contract
-     * @param _stop true disables alterting the contract
-     */
-    function setStopped(bool _stop) external onlyController {
-        stopped = _stop;
-        emit Stopped(_stop);
-    }
-
-    /**
-     * @notice Obtain public message content hash
-     * @param _who Account reading the message from.
-     * @return contenthash of user custom manifest, or if unset, default manifest
-     */
-    function getManifest(address _who) external view returns(bytes memory manifest) {
-        manifest = manifests[_who];
-        if(manifest.length == 0) {
-            manifest = manifests[address(0)];
-        }
-    }
-
-    /** 
-     * @dev changes storage and fires event 
-     * @param account account being changed, use address(0) for default manifest
-     * @param manifest contenthash format data.
-     */
-    function updateManifest(address account, bytes memory manifest) internal {
-        manifests[account] = manifest;
-        emit Manifest(account, manifest);
+    function getTribute(address _of) external view
+        returns (uint256)
+    {
+        return tributeCatalog[_of];
     }
 
 }
