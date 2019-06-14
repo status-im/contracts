b3a10fc20f6474c22d1bf2e48e6c9d7b47750089
Ricardo Guilherme Schmidt <ricardo3@status.im>
2019-06-14 08:14:27 -0300

    fix delegatedcall

--- contracts/deploy/DelegatedCall.sol
+++ contracts/deploy/DelegatedCall.sol
@@ -3,7 +3,7 @@ pragma solidity >=0.5.0 <0.6.0;
 
 /**
  * @title DelegatedCall
- * @author Ricardo Guilherme Schmidt (Status Research & Development GmbH) 
+ * @author Ricardo Guilherme Schmidt (Status Research & Development GmbH)
  * @dev Encapsulates delegatecall related logic.
  */
 contract DelegatedCall {
@@ -19,18 +19,18 @@ contract DelegatedCall {
      */
     modifier delegateAndReturn(address _target) {
         if(_target == address(0)) {
-            _; //normal execution 
+            _; //normal execution
         } else {
             //delegated execution
             bytes memory returnData;
             bool success;
             (success, returnData) = _target.delegatecall(msg.data);
-            require(success, "Delegated Call failed"); 
-
             //exit-return delegatecall returnData
             assembly {
-                return(add(returnData, 0x20), returnData) 
+                switch success case 0 { revert(add(returnData, 0x20), mload(returnData)) }
+                default { return(add(returnData, 0x20), mload(returnData)) }
             }
+
         }
     }
 
